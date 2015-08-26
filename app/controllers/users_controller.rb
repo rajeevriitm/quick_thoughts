e58d2db7c4db7bb9e57cb19b0cfd6b2731cd4736
rajeev <rajeevriitm@gmail.com>
2015-08-26 06:48:56 +0530

    propic added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,31 @@ class UsersController < ApplicationController
   def edit
     @user=User.find_by(id: params[:id])
   end
-  def create
-    @user=User.new(user_params)
-    if @user.save
-      login @user
-      flash[:info]="Welcome to QuickThoughts"
-      redirect_to user_path(@user)
-    else
-      render 'static_pages/home'
-    end
+  def update
+  @user=User.find(params[:id])
+  if @user.update_attributes(user_params)
+    flash[:info]="Successfully edited"
+    redirect_to home_path
+  else
+    render 'edit'
   end
-  def show
-    @user=User.find_by(id: params[:id])
+end
+def create
+  @user=User.new(user_params)
+  if @user.save
+    login @user
+    flash[:info]="Welcome to QuickThoughts"
+    redirect_to user_path(@user)
+  else
+    render 'static_pages/home'
   end
+end
+def show
+  @user=User.find_by(id: params[:id])
+end
 
-  private
-  def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation,:about,:picture)
-  end
+private
+def user_params
+  params.require(:user).permit(:name,:email,:password,:password_confirmation,:about,:picture)
+end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,14 +2,17 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [300, 300]
+  process resize_to_fill: [200, 200]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+end  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
