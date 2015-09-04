872cf1de8705b2b80a8c5607b445ba6d442753eb
rajeev <rajeevriitm@gmail.com>
2015-09-04 18:17:22 +0530

    sad

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 User.create!(name:"Rajeev Ramakrishnan",email:"rajeevriitm@gmail.com",
   password:"sadsad",password_confirmation:"sadsad", about: "web developer")
-50.times do |n|
+10.times do |n|
   name=Faker::Name.name
   email = "Rajeev-#{n+1}@railstutorial.org"
   User.create!(name:name,email:email,password:"password",password_confirmation:"password", about: "beginner")
 end
 users=User.order(:created_at).take(6)
-30.times do
+20.times do
   users.each do |user|
     content=Faker::Lorem.paragraph(sentence_count= 10)
     title=Faker::Lorem.paragraph(sentence_count= 1)
